101a0a8e0c30ae1982a2ee03d225fd84f82afe4a
erikivanmelo <erikivanmelog@gmail.com>
2024-01-01 18:12:19 -0300

    correccion y eliminacion de calculos de vector2d

--- src/Physics/Vector2D.h
+++ src/Physics/Vector2D.h
@@ -30,28 +30,16 @@ public:
     }
 
     
-    bool operator<(const Vector2D& v2){
-        return ( this->x < v2.x && this->y < v2.y);
-    }
-
-    bool operator<(const float& v2){
-        return ( this->x < v2 && this->y < v2);
-    }
-
     bool operator!=(const float& v2){
-        return ( this->x != v2 && this->y != v2);
+        return ( this->x != v2 || this->y != v2);
     }
 
     bool operator!=(const Vector2D& v2){
-        return ( this->x != v2.x && this->y != v2.y);
+        return ( this->x != v2.x || this->y != v2.y);
     }
 
     bool operator==(const Vector2D& v2){
         return ( this->x == v2.x && this->y == v2.y);
-    }
-
-    bool operator<(const int& v2){
-        return ( this->x < v2 && this->y < v2);
     }
 
     inline Vector2D operator-(const Vector2D& v2){
